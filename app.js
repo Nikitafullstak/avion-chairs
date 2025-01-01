0093962bcb0ee06d97d0919d5c2850249dda8e2d
Nikitafullstak <n4963959@gmail.com>
2025-01-01 12:13:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+function tap(){
+    const el = document.querySelector('.bun')
+    el.innerHTML = "hello people"
+}
+function tabl(){
+    let elem = document.getElementById('imh3');
+    let style = getComputedStyle(elem);
+    if (style.display === 'none'){
+        document.getElementById('imh3').style.display='block';
+
+    }else if (style.display !== 'none'){
+        document.getElementById('imh3').style.display='none'
+    }
+}
+function terb(){
+    let set = document.getElementById('her4');
+    let style = getComputedStyle(set);
+    if (style.display === 'none'){
+        document.getElementById('her4').style.display='block';
+
+    }else if (style.display !== 'none'){
+        document.getElementById('her4').style.display='none'
+    }
+}
+function her(){
+    let seter = document.getElementById('her5');
+    let style = getComputedStyle(seter);
+    if (style.display === 'none'){
+        document.getElementById('her5').style.display='block';
+
+    }else if (style.display !== 'none'){
+        document.getElementById('her5').style.display='none'
+    }
+}
+function tabltr(){
+    let setik = document.getElementById('imh6');
+    let style = getComputedStyle(setik);
+    if (style.display === 'none'){
+        document.getElementById('imh6').style.display='block';
+
+    }else if (style.display !== 'none'){
+        document.getElementById('imh6').style.display='none'
+    }
+}
+
+
+
+
